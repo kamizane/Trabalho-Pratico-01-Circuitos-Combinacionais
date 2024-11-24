4a4d58055e1a0797436f31d5ac7bcaf5fc304695
kamizane <julioitabira@hotmail.com>
2024-11-24 19:06:16 -0300

    tp corrigido quanto ao fora do intervalo

--- Modulo_display/Codigo/display.v
+++ Modulo_display/Codigo/display.v
@@ -6,7 +6,17 @@ module display_7_seg (b1,b2,b3,b4,b5,b_par,A,B,C,D,E,F,G);
 
     verificador_paridade verifica(b1,b2,b3,b4,b5,b_par,S);
     always@(*) begin
-        if ((b1 == 1'b1 & b2 == 1'b1) | b1 == 1'b1 & b3 == 1'b1)begin
+        if(S == 1'b0)begin
+            A <= 1'b1;
+            B <= 1'b1;
+            C <= 1'b1;
+            D <= 1'b1;
+            E <= 1'b1;
+            F <= 1'b1;
+            G <= 1'b1;
+
+        end
+        else if ((b1 == 1'b1 & b2 == 1'b1) | b1 == 1'b1 & b3 == 1'b1)begin
             A <= 1'b0;
             B <= 1'b0;
             C <= 1'b0;
@@ -23,15 +33,6 @@ module display_7_seg (b1,b2,b3,b4,b5,b_par,A,B,C,D,E,F,G);
             E <= (((~b1) & b2 & (~b3)) | ((~b1) & b4 & (~b5)) | ((~b2) & (~b3) & (~b4)) | ((~b1) & (~b2) & (~b4) & b5) | (b1 & (~b2) & (~b3) & b5));
             F <= (((~b1) & b2 & (~b4)) | ((~b1) & b2 & b5) | ((~b1) & b3 & b4) | ((~b1) & (~b2) & b4 & (~b5)) | ((~b1) & (~b3) & (~b4) & b5) | (b1 & (~b2) & (~b3) & (~b5)) | (b1 & (~b2) & (~b3) & b4));
             G <= (((~b1) & (~b4) & b5) | ((~b1) & b3 & b5) | ((~b1) & b2 & b5) | ((~b1) & (~b2) & b4 & (~b5)) | ((~b1) & b2 & b3 &(~b4)) | (b1 & (~b2) & (~b3) & (~b4)));
-        end
-        else begin
-            A <= 1'b1;
-            B <= 1'b1;
-            C <= 1'b1;
-            D <= 1'b1;
-            E <= 1'b1;
-            F <= 1'b1;
-            G <= 1'b1;
         end
     end
 endmodule
