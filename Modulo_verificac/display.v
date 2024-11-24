a9f87f1fddaaf4393eaaef3a2ed41dccb4229279
kamizane <julioitabira@hotmail.com>
2024-11-24 10:09:56 -0300

    falta organizar em pastas

---
+++ Modulo_verificac/display.v
@@ -0,0 +1,9 @@
+module display-7_seg (b1,b2,b3,b4,b5,b_par,A,B,C,D,E,F,G);
+    input b1,b2,b3,b4,b5,b_par;
+    output reg A,B,C,D,E,F,G;
+
+    always@(*) begin
+        if
+        A <=
+    end
+endmodule
